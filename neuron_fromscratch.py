0cfdd3fa617ac5a385e33a0d675a8638fd3f3f2e
Om Jadhav <omjadha9728@gmail.com>
2026-09-15 21:48:45 +0530

    Clarify neuron function with a comment
    Added comment to clarify the activation function of the neuron.

--- neuron_fromscratch.py
+++ neuron_fromscratch.py
@@ -1,11 +1,8 @@
 import numpy as np
 import random
 
-
 def neuron(x, w, b):
-  return np.dot(x, w) + b
-
-
+  return np.dot(x, w) + b  # initially the neurons have a linear activation function
 
 x = np.random.randint(0, 3,size = (3,1))
 w = np.random.randint(0, 3, size = (1,3))
