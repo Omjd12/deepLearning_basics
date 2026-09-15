70e0d4108a7a8d2bb8eb8e2873780836354840c2
Om Jadhav <omjadha9728@gmail.com>
2026-09-15 21:15:16 +0530

    Implement basic neuron function in Python

---
+++ neuron_fromscratch.py
@@ -0,0 +1,13 @@
+import numpy as np
+import random
+
+
+def neuron(x, w, b):
+  return np.dot(x, w) + b
+
+
+
+x = np.random.randint(0, 3,size = (3,1))
+w = np.random.randint(0, 3, size = (1,3))
+result = neuron(x , w, 1)
+print(result)
