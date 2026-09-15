b780c58abab2e3d367080c7a73b6c13deefcfaf3
Om Jadhav <omjadha9728@gmail.com>
2026-09-15 22:43:11 +0530

    added activation function to the neurons

--- neuron_fromscratch.py
+++ neuron_fromscratch.py
@@ -1,10 +1,28 @@
 import numpy as np
 import random
 
-def neuron(x, w, b):
-  return np.dot(x, w) + b  # initially the neurons have a linear activation function
+def sigmoid(z):
+  return 1/(1+np.exp(-z))
+
+def relu(z):
+  return np.maximum(0, z)
+
+def tanh(z):
+  return np.tanh(z)
+
+def neurons(x, w, b, Activation = None):
+  z = np.dot(x, w) + b
+  if Activation is None:
+    return z
+  return Activation(z)
 
 x = np.random.randint(0, 3,size = (3,1))
 w = np.random.randint(0, 3, size = (1,3))
-result = neuron(x , w, 1)
+result = neurons(x,w, 4)
+print(result)
+result = neurons(x , w, 1, Activation= tanh)
+print(result)
+result = neurons(x, w, 2, Activation= relu)
+print(result)
+result = neurons(x, w, 1, Activation= sigmoid)
 print(result)
